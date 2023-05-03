d1270af1b4fdb42ebc7b40a82166cc0a8c09889d
Protap Ghosh <protapg518@gmail.com>
2023-05-03 12:37:14 +0600

    sending json data

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+
+const chefDetails = require("./data/chef-details.json");
+const recipe = require("./data/recipe.json");
+
 app.use(cors());
 
 app.get('/', (req, res)=> {
     res.send("Clever Cookie is running...");
 });
+
+app.get("/chef-details", (req, res) => { 
+    res.send(chefDetails);
+})
+
+app.get("/recipe", (req, res) => {
+    res.send(recipe);
+})
+
+
 
 app.listen(port, () => {
     console.log(`Clever Cookie is running on port: ${port}`);
